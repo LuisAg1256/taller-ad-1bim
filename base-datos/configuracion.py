8e832387523b9903cad455d3bb46c35608090f18
LuisAg1256 <laaguilar21@utpl.edu.ec>
2026-06-02 12:44:04 -0500

    Evidencias

--- base-datos/configuracion.py
+++ base-datos/configuracion.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
-cadena_base_datos = 'postgresql+psycopg2://user:password@localhost:5434/postgres' # Cadena de postgres
-#cadena_base_datos = 'mysql+pymysql://root:rootpassword@localhost:3308/universidad' #Cadena de mariadb
+#cadena_base_datos = 'postgresql+psycopg2://user:password@localhost:5434/postgres' # Cadena de postgres
+cadena_base_datos = 'mysql+pymysql://root:rootpassword@localhost:3308/universidad' #Cadena de mariadb
 
 # cadena_base_datos = 'sqlite:///universidad.db'  #Cadena de sqlite
 
